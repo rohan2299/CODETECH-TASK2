653878eb8e8a4df3dcb9d4efde2d695ee7c1fe58
Rohan Jadhav <119944474+rohan2299@users.noreply.github.com>
2024-10-25 19:01:44 +0530

    Add files via upload

---
+++ TechstackList.js
@@ -0,0 +1,70 @@
+import { BsFiletypeHtml, BsFiletypeCss, BsBootstrap } from "react-icons/bs";
+import { FaNodeJs } from "react-icons/fa";
+import { TbBrandNextjs } from "react-icons/tb";
+import {
+  SiJavascript,
+  SiMui,
+  SiReact,
+  SiTailwindcss,
+  SiMongodb,
+  SiGithub,
+  SiFirebase,
+  SiExpress,
+} from "react-icons/si";
+export const TechstackList = [
+  {
+    _id: 1,
+    name: "HTML",
+    icon: BsFiletypeHtml,
+  },
+  {
+    _id: 2,
+    name: "CSS",
+    icon: BsFiletypeCss,
+  },
+  {
+    _id: 3,
+    name: "Bootstrap",
+    icon: BsBootstrap,
+  },
+  {
+    _id: 4,
+    name: "Javascript",
+    icon: SiJavascript,
+  },
+  {
+    _id: 5,
+    name: "React JS",
+    icon: SiReact,
+  },
+  {
+    _id: 6,
+    name: "Github",
+    icon: TbBrandNextjs,
+  },
+  {
+    _id: 7,
+    name: "Firebase",
+    icon: SiMui,
+  },
+  {
+    _id: 8,
+    name: "Tailwind ",
+    icon: SiTailwindcss,
+  },
+  {
+    _id: 9,
+    name: "Node JS ",
+    icon: FaNodeJs,
+  },
+  {
+    _id: 10,
+    name: "Express JS ",
+    icon: SiExpress,
+  },
+  {
+    _id: 11,
+    name: "Mongodb ",
+    icon: SiMongodb,
+  }
+];
